a80003ac7f77744e4e4c0ed4c0bf6151040e9e42
Luca Bonini <luca14bonini@gmail.com>
2024-03-02 11:23:15 -0800

    Final fixes on the db file and the migrations

---
+++ Migrations/20240302171156_ThirdCreate.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bangazon.Migrations
+{
+    public partial class ThirdCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "OrderProducts",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CategoryId",
+                value: 2);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "OrderProducts",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CategoryId",
+                value: 1);
+        }
+    }
+}
