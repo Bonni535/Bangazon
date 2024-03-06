5b41bfa46849c9becdcf29ffd9904cf1cc69cc2f
Luca Bonini <luca14bonini@gmail.com>
2024-03-05 19:59:16 -0800

    Changes

--- Models/OrderProducts.cs
+++ Models/OrderProducts.cs
@@ -4,7 +4,7 @@ namespace Bangazon.Models;
 
 public class OrderProducts
 {
-    public int Id { get; set; }
+    public int? Id { get; set; }
     public int OrderId { get; set; }
     public int ProductId { get; set; }
 }
